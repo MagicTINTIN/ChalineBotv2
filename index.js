72aac602a80351cd1dfc33f11e01276ae1ae4f26
ALC-ProduXion <compte.poubelle.georges@gmail.com>
2022-10-01 00:24:35 +0200

    add .vscode file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("Starting Chaline...\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
 
 // Import librairies
+const { importer } = require("./internals");
+importer("nothing");
+
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 // Client creation and export
 const client = new Client({
@@ -32,6 +35,24 @@ exports.client = client;
 // when Chaline logged in Discord
 client.once('ready', () => {
     console.log("___________________\nChaline is ready");
+});
+
+// Prevents bot from crash
+process.on('uncaughtException', function (err) {
+    console.error(err);
+
+    logger.all("📌 Un chat m'a coupé la route, mais je suis indemne");
+
+    try {
+        errorwide = err.stack
+        messerror = errorwide.match(/(.{1,1800})/g);
+        for (const msgparterror of messerror) {
+            if (initialized)
+                logger.channel("```diff\n-> " + msgparterror + "\n```");
+        }
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 client.login(process.env.TOKEN);
---
+++ internals/importer.js
@@ -0,0 +1,6 @@
+const importer = (arg) => {
+    // Imports other things
+    console.log(`Imported 0 elements from importer : ${arg}`);
+};
+
+module.exports = importer;
